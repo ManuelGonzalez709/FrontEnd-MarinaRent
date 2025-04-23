8cd30b5a2f91b415b1b82534235a78da09284c29
manuelgonz <102143690+ManuelGonzalez709@users.noreply.github.com>
2025-04-23 08:14:11 +0200

    Borrado de Imagenes que no usamos
    Borrado masivo de Imagenes que no usamos

--- src/pages/Alquilables.jsx
+++ src/pages/Alquilables.jsx
@@ -31,6 +31,8 @@ export default function Informativos() {
     }
 
     return (
+        <>
+
         <div className="bg-white">
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
 
@@ -71,5 +73,6 @@ export default function Informativos() {
                 )}
             </div>
         </div>
+        </>
     );
 }
