4de782b4f26959d51c71421206f346ec5ee305bf
manuelgonz <manuel5365.mgl@gmail.com>
2025-05-22 12:19:52 +0200

    Cambios de Local a Remoto y viceversa

--- src/utilities/apirest.js
+++ src/utilities/apirest.js
@@ -1,4 +1,11 @@
-//export const API_URL = "http://localhost:8000/";
+//Hosting https://manu.cicloflorenciopintado.es
+
 const URLSERVER = "https://manu.cicloflorenciopintado.es/laravel/";
 export const API_URL = URLSERVER+"public/"
 export const IMAGE_URL = URLSERVER+"storage/app/public/photos/";
+
+
+//Hosting Local
+//const URLSERVER = "http://localhost:8000/";
+//export const API_URL = URLSERVER;
+//export const IMAGE_URL = URLSERVER+"storage/photos/";
